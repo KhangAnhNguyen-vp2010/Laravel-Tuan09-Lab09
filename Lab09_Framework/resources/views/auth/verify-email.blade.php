19fa32a1404feb159b3f23aa58706cd3c8a3d85d
Anh Khang <taicuoi664@gmail.com>
2025-11-18 20:55:35 +0700

    Push Bai Tap 07

--- Lab09_Framework/resources/views/auth/verify-email.blade.php
+++ Lab09_Framework/resources/views/auth/verify-email.blade.php
@@ -7,12 +7,12 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Cảm ơn bạn đã đăng ký! Trước khi bắt đầu, vui lòng xác minh địa chỉ email bằng cách nhấn vào liên kết chúng tôi vừa gửi. Nếu chưa nhận được email, bạn có thể yêu cầu gửi lại.') }}
         </div>
 
         @if (session('status') == 'verification-link-sent')
             <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                {{ __('Một liên kết xác minh mới đã được gửi đến địa chỉ email bạn đăng ký.') }}
             </div>
         @endif
 
@@ -22,7 +22,7 @@
 
                 <div>
                     <x-button>
-                        {{ __('Resend Verification Email') }}
+                        {{ __('Gửi lại email xác minh') }}
                     </x-button>
                 </div>
             </form>
@@ -31,7 +31,7 @@
                 @csrf
 
                 <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
+                    {{ __('Đăng xuất') }}
                 </button>
             </form>
         </div>
